0428df77bbd36a57dc1e2b2a13893c32e9bb6bd2
Chuntian Xu <xuspring.2021@gmail.com>
2022-01-01 02:28:51 +0800

    update the source code
    Signed-off-by: Chuntian Xu <xuspring.2021@gmail.com>

---
+++ inet4/src/inet/networklayer/ipv4/Ipv4HeaderSerializer.h
@@ -0,0 +1,30 @@
+// src/inet/networklayer/ipv4/Ipv4HeaderSerializer.h
+
+#ifndef __INET_IPV4HEADERSERIALIZER_H
+#define __INET_IPV4HEADERSERIALIZER_H
+
+#include "inet/common/packet/serializer/FieldsChunkSerializer.h"
+#include "inet/networklayer/ipv4/Ipv4Header_m.h"
+
+namespace inet {
+
+/**
+ * Converts between Ipv4Header and binary (network byte order) Ipv4 header.
+ */
+class INET_API Ipv4HeaderSerializer : public FieldsChunkSerializer
+{
+  protected:
+    virtual void serializeOption(MemoryOutputStream& stream, const TlvOptionBase *option) const;
+    virtual TlvOptionBase *deserializeOption(MemoryInputStream& stream) const;
+
+    virtual void serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const override;
+    virtual const Ptr<Chunk> deserialize(MemoryInputStream& stream) const override;
+
+  public:
+    Ipv4HeaderSerializer() : FieldsChunkSerializer() {}
+};
+
+} // namespace inet
+
+#endif // ifndef __INET_IPV4HEADERSERIALIZER_H
+
